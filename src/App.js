4fce8b799b33a2e106d293ffa2d7d7c36e8d1651
Jerlyn <jerlynthomas@gmail.com>
2025-03-21 00:10:54 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NumberGame from './NumberGame';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberGame />
+    </div>
+  );
+}
+
+export default App;
